a15d0d0fba4395493826e1c6b95306cc2c730b89
Hyunwoo Jung <chaosphch@gmail.com>
2022-05-29 17:19:55 +0900

    First commit

---
+++ code/eval.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def reconstructionError(predicted_joints, gt_joints): 
+
+  
+  return error
---
+++ code/hw3.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import cv2
+
+
+def find_bbox(keypoints): 
+
+    return bbox
+
+def drawKeyPoints(img, keypoints, color):
+
+
+
+def drawMesh(img, vertices, faces, lineColor): 
+
+
+
+def visSkeleton(img, joints): 
+
+
+
+def drawJoints(img, joints, links, lineColor, keypointColor):
